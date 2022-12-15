0c2f49395674aacd1bc228752fe5fe6c4e775d4c
MassylHAMDI <116282135+MassylHAMDI@users.noreply.github.com>
2022-12-15 21:24:30 +0100

    Add files via upload

--- Fonctions.py
+++ Fonctions.py
@@ -6,19 +6,21 @@ import numpy as np
 
 
 # Fonction qui retourne le résidu 
-def R(i, param):
+def Residu(i, param):
     L1, L2, x, y  = param
     a,b = i[0],i[1]
     return [L1*np.cos(a)+L2*np.cos(a+b)-x,L1*np.sin(a)+L2*np.sin(a+b)-y]
 
 
+#____________________________________________________________________
 # Retourne le carrée de la norme du résidu
-def R2(i, param):
-    return np.linalg.norm(R(i,param))**2
+def Residu_2(i, param):
+    return np.linalg.norm(Residu(i,param))**2
 
 
+#____________________________________________________________________
 # Fonction qui retourne le gradient de la norme du residu R2
-def dR2(i,param):
+def dResidu_2(i,param):
     L1, L2, x, y  = param
     a,b = i[0],i[1]
     dx = 2 * L1 * x * np.sin(a) - 2 * L1 * y * np.cos(a) + 2 * L2 * x * np.sin(a + b) - 2 * L2 * y * np.cos(a + b)
@@ -26,6 +28,7 @@ def dR2(i,param):
     return dx, dy 
 
 
+#____________________________________________________________________
 # calcule de la matrice hessienne 
 def H(i,param):
     L1, L2, x, y  = param
@@ -38,8 +41,8 @@ def H(i,param):
                      [dyx, dyy]])
     return Hess 
     
-
-# Méthode de Gradient:
+#____________________________________________________________________
+# Méthode de Gradient avec le mécanisme qui ajuste alpha:
 def grad_fixe(J,DJ, x0, y0, alpha, eps, nmax, param):
 
     """
@@ -66,12 +69,17 @@ def grad_fixe(J,DJ, x0, y0, alpha, eps, nmax, param):
     # n nombre d'iteration
     n = 0
     # Tableau qui contiendra le valeur de x
-    point_X = []
+    point_X = [xn]
 
     # Tableau qui contiendra le valeur de x
-    point_Y = []
+    point_Y = [yn]
     
-    
+    # initialisé alpha à une const
+    alpha_security = alpha
+
+    # un indicateur pour savoir si le programme à convergé 
+    etat = True
+
     # tant que dX est plus grand que la précision = eps
     # n ne dépasse pas le num d'itérations max notre boucle tourne 
     while (dX > eps and n < nmax):
@@ -81,11 +89,18 @@ def grad_fixe(J,DJ, x0, y0, alpha, eps, nmax, param):
 
         f_num = J([xn, yn],param)
         
+        alpha = alpha_security
+
+        # mécanisme qui ajuste si besoin la valeur de alpha
+        if J([xn - (alpha * gradx), yn - (alpha * grady)],param) >= J([xn, yn],param):
+            alpha = alpha * 0.05 
+
         # Calcule des nouveaux points 
         xn = xn - (alpha * gradx)
         yn = yn - (alpha * grady)
         
         f_res = J([xn, yn],param)
+
         
         # On ajoute xn à la liste à chaque iteration
         point_X.append(xn)
@@ -102,20 +117,112 @@ def grad_fixe(J,DJ, x0, y0, alpha, eps, nmax, param):
     # indicateur de convergence 
     # si notre programme ne converge pas on aura un message d'erreur dans la console principale 
     if (n >= nmax):
+        etat = False
         print( f"Le programme ne converge pas")
 
     # on retourne la liste des x et y 
     # on retourne le nombre d'iteration réaliser 
-    return [point_X, point_Y], n 
+    return [point_X, point_Y], n , etat
 
 
-# Méthode de Newton
-def Newton(J, DJ, HJ, x0, y0, err, n_max, param):
-    #Xn initialisé à x0
-    xn=x0
+# Méthode de Gradient sans le mécanisme qui ajuste alpha:
+def grad_fixe_sansmeca(J,DJ, x0, y0, alpha, eps, nmax, param):
+
+    """
+    Notations:
+    J : le carrée de la norme du résidu
+    Dj : le gradient de la norme du résidu
+    Xn et Yn : point initial  l’itération n.
+    dX : pas de déplacement pour une itération.
+    n et nmax : compteur d’itérations et nombre maximal d’itérations autorisé. 
+    X0 et Y0 : point de départ de l’algorithme.
+    alpha :  pas de recherche.
+    eps : : critère de précision.
+
+    """
+    # on initialise Xn à x0
+    xn = x0
+
+    # on initialise Yn à Y0
+    yn = y0
+
+    # dX initialisé à 1 pour entrer dans la boucle
+    dX = 1
+
+    # n nombre d'iteration
+    n = 0
+    # Tableau qui contiendra le valeur de x
+    point_X = [xn]
+
+    # Tableau qui contiendra le valeur de x
+    point_Y = [yn]
     
-    #Yn initialisé a y0
-    yn=y0
+
+    # un indicateur pour savoir si le programme à convergé 
+    etat = True
+
+    # tant que dX est plus grand que la précision = eps
+    # n ne dépasse pas le num d'itérations max notre boucle tourne 
+    while (dX > eps and n < nmax):
+        
+        # calcule du gradient de la norme du résidu R2 pour les valeurs xn et yn 
+        gradx, grady = DJ([xn, yn],param)
+
+        f_num = J([xn, yn],param)
+        
+
+        # Calcule des nouveaux points 
+        xn = xn - (alpha * gradx)
+        yn = yn - (alpha * grady)
+        
+        f_res = J([xn, yn],param)
+
+        
+        # On ajoute xn à la liste à chaque iteration
+        point_X.append(xn)
+
+        # On ajoute xn à la liste à chaque iteration
+        point_Y.append(yn)
+        
+        # Calcule de la nouvelle valeur de dX
+        dX = abs(f_res - f_num)
+
+        # on incrémente n
+        n = n + 1
+        
+    
+    # indicateur de convergence 
+    # si notre programme ne converge pas on aura un message d'erreur dans la console principale 
+    if (n >= nmax):
+        etat = False
+        print( f"Le programme ne converge pas")
+
+    # on retourne la liste des x et y 
+    # on retourne le nombre d'iteration réaliser 
+    return [point_X, point_Y], n , etat
+
+
+#____________________________________________________________________
+# Méthode de Newton
+def Newton(J, DJ, HJ, x0, y0, eps, nmax, param):
+    """
+    Notations:
+    J : le carrée de la norme du résidu
+    Dj : le gradient de la norme du résidu
+    Hj : La matrice hessienne du résidu
+    Xn et Yn : point initial  l’itération n.
+    dX : pas de déplacement pour une itération.
+    n et nmax : compteur d’itérations et nombre maximal d’itérations autorisé. 
+    X0 et Y0 : point de départ de l’algorithme.
+    eps : : critère de précision.
+
+    """
+
+    # on initialise Xn à x0
+    xn = x0
+
+    # on initialise Yn à Y0
+    yn = y0
     
     #dX initialisé à 1
     dX = 1    
@@ -125,37 +232,94 @@ def Newton(J, DJ, HJ, x0, y0, err, n_max, param):
     
     #on initialise une liste avec la premiere valeur de xn 
     #qu'on augmentera au file de la boucle 
-    Liste1 = [x0]
-    Liste2 = [y0]
+    Listex = [xn]
+    Listey = [yn]
     
-    Liste3 = [J([xn, yn],param)]
+    # un indicateur pour savoir si le programme à convergé 
+    etat = True
     
     #tant que dX est plus grand que la précision = eps et qu'on a
     #pas dépassé le num d'itérations
-    while(dX > err and n < n_max):
+    while(dX > eps and n < nmax):
         
+        # calcule du gradient de la norme du résidu R2 pour les valeurs xn et yn 
         gradx, grady = DJ([xn,yn],param)
+
+        # On le transforme en numpy array
         grad = np.array([gradx,grady])
         
-        hess = HJ([xn,yn],param)
-        inv_hess = np.linalg.inv(hess)
+        # calcule de la matrice hessienne
+        hessienne = HJ([xn,yn],param)
+        inv_hessienne = np.linalg.inv(hessienne)
         
-        delta = np.matmul(-1*grad,inv_hess)
+        # Calcule de delta  delta = -grad * inv(hess)
+        delta = np.matmul(-1*grad,inv_hessienne)
+
+        # calcule de xn et yn pour chaque iteration
         xn = xn + delta[0]   
         yn = yn + delta[1]
+
+        # On ajoute xnet yn à la liste à chaque iteration
+        Listex.append(xn)
+        Listey.append(yn)
         
-        Liste1.append(xn)
-        Liste2.append(yn)
-        Liste3.append(J([xn, yn],param))
-        
-        #dX = la norme de delta
+        # calcule de la norme de delta
         dX = np.linalg.norm(delta)
         
         #on incrémente n
         n = n+1
-        
-        
-        list = [Liste1,Liste2,Liste3]
-        
-    return list
+
+    # indicateur de convergence 
+    # si notre programme ne converge pas on aura un message d'erreur dans la console principale 
+    if (n >= nmax):
+        etat = False
+        print( f"Le programme ne converge pas")
+
+    # on retourne la liste des x et y 
+    # on retourne le nombre d'iteration réaliser   
+    return [Listex,Listey],n,etat
+
+
+#____________________________________________________________________
+# Cette Fonction nous permet d'avoir une droite 
+def Trajectoire(xydepart,xyfinal):
+    x_start , y_start = xydepart
+    x_final , y_final = xyfinal
+    
+    a = (y_final - y_start)/(x_final - x_start)
+    b = y_start - a * x_start 
+    x = np.linspace(x_start, x_final, 8)
+    y = a*x - b
+    
+    return x,y
+
+
+#____________________________________________________________________
+# Cette fonction calcule la position x,y du bras articulé 
+def position(theta1,theta2):
+    x = []
+    y = []
+
+    L1 = 2
+    L2 = 2
+    # ici on suppose que len(theta1) est égal à que len(theta2)
+    for i in range(len(theta1)):
+
+        x_ = np.cumsum([0,
+                    L1 * np.cos(theta1[i]),
+                    L2 * np.cos(theta1[i]+theta2[i])])
+                    
+        y_ = np.cumsum([0,
+                    L1 * np.sin(theta1[i]),
+                    L2 * np.sin(theta1[i]+theta2)[i]])
+
+        x.append(x_)
+        y.append(y_)
+
+    # Pour facilité la manipulation des tableaux on les mets en array
+    x = np.array(x)
+    y = np.array(y) 
+
+    # retourne listes des position x et y   
+    return x,y
 
